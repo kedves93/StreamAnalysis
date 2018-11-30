332d4383a60f39cc8f970d3c4fa0f848b7624307
Szilard Kedves <szilard.kedves@siemens.com>
2018-12-01 00:24:15 +0200

    Major refactoring in the applications

---
+++ Applications/DotNetEC2/DotNetEC2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace DotNetEC2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x => Console.WriteLine(x));
+            Console.ReadKey();
+        }
+    }
+}
