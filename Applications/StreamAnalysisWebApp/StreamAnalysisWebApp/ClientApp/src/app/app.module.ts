71a2cc5eb94731632016fac1b1ac21656a7af375
Szilard Kedves <szilard.kedves@siemens.com>
2018-12-04 15:03:22 +0200

    Made progress on the web app home and login pages

--- Applications/StreamAnalysisWebApp/StreamAnalysisWebApp/ClientApp/src/app/app.module.ts
+++ Applications/StreamAnalysisWebApp/StreamAnalysisWebApp/ClientApp/src/app/app.module.ts
@@ -1,21 +1,32 @@
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Applications/StreamAnalysisWebApp/StreamAnalysisWebApp/ClientApp/src/app/services/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getUserDetails() {
+    // post these deatils to API server, then return user info
+  }
+}
