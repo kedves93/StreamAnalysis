b257a60ea4f25f60d7ba354f5f5755822a07ad08
Szilard Kedves <szilard.kedves@siemens.com>
2019-05-20 15:55:15 +0300

    deleted unused apps

--- Applications/(OBSOLETE)StreamAnalysisWebApp/StreamAnalysisWebApp/Models/AwsDevCredentials.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StreamAnalysisWebApp.Models
-{
-    public class AwsDevCredentials
-    {
-        public string AwsAccessKeyId { get; set; }
-
-        public string AwsSecretAccessKey { get; set; }
-    }
-}
--- Applications/(OBSOLETE)StreamAnalysisWebApp/StreamAnalysisWebApp/Models/SignInUserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StreamAnalysisWebApp.Models
-{
-    public class SignInUserInfo
-    {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-    }
-}
