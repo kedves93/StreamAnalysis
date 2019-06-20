b26f5f0f15134a0b3074413e58f622d7a4718ce9
Szilard Kedves <szilard.kedves@siemens.com>
2019-06-20 17:48:56 +0300

    SignalR updates

--- Applications/WebApplication/WebApplication/Hubs/TopicsHub.cs
+++ Applications/WebApplication/WebApplication/Hubs/TopicsHub.cs
@@ -1,8 +1,42 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using WebApplication.Interfaces;
 
 namespace WebApplication.Hubs
 {
-    public class TopicsHub : Hub
+    public class TopicsHub : Hub<ITopicClient>
     {
+        private readonly IActiveMQService _activeMQService;
+
+        private readonly ILogger _logger;
+
+        public TopicsHub(IActiveMQService activeMQService, ILogger<TopicsHub> logger)
+        {
+            _activeMQService = activeMQService;
+            _logger = logger;
+        }
+
+        public async Task StartRealTimeMessagesFromTopic(string topic)
+        {
+            _activeMQService.OnTopicMessage += async (sender, e) => await Clients.Caller.OnNewMessageArrived(e.Message);
+            await _activeMQService.StartListeningOnTopicAsync(topic);
+        }
+
+        public async Task StopRealTimeMessagesFromTopic(string topic)
+        {
+            await _activeMQService.StopListeningOnTopicAsync(topic);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Task.Run(() => _logger.LogInformation("Connection with hub started. Id: " + Context.ConnectionId));
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Task.Run(() => _logger.LogError("Hub connection disconnected. Error: " + ex.Message));
+        }
     }
 }
---
+++ Applications/WebApplication/WebApplication/Interfaces/ITopicClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Interfaces
+{
+    /// <summary>
+    /// SignalR uses RPC (Remote Procedure Call) which means that methods called on the server are executed on the client side.
+    /// This interface strongly types those client side methods.
+    /// </summary>
+    public interface ITopicClient
+    {
+        /// <summary>
+        /// The implementation of this method is on client side (RPC).
+        /// </summary>
+        /// <param name="topicMessage"></param>
+        /// <returns></returns>
+        Task OnNewMessageArrived(TopicMessage topicMessage);
+    }
+}
